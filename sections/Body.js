c1e5d7a1a55c9f20384a18e28841a5871b4e5eb4
David L. Herring <david@fai2.com>
2022-01-17 22:27:08 -0500

    very beginnings of CSS processing

--- sections/Body.js
+++ sections/Body.js
@@ -1,8 +1,13 @@
-import { html } from "../lib/html-template-tag";
+import { css, html } from "../lib/html-template-tag";
 import { Header } from "./Header";
-
 
 export const Body = () => html`
     ${Header}
     <p>More to come!</p>
+`;
+
+export const BodyStyles = () => css`
+    body {
+        font-family: sans-serif;
+    }
 `;
