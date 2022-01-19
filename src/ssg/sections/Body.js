8a603b050ec6634d4146059ee9b6f464618ffcc8
David L. Herring <david@fai2.com>
2022-01-18 21:39:21 -0500

    dev server works, but build still doesn't

---
+++ src/ssg/sections/Body.js
@@ -0,0 +1,13 @@
+import { css, html } from "../../../lib/html-template-tag";
+import { Header } from "./Header";
+
+export const Body = () => html`
+    ${Header}
+    <p>More to come...</p>
+`;
+
+export const style = css`
+    body {
+        font-family: sans-serif;
+    }
+`;
