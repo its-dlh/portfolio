3fce7be2e6f06bf5422f391822e2388cc420db00
David L. Herring <david@fai2.com>
2022-01-24 21:31:25 -0500

    build is working better and better

--- src/ssg/sections/Header.js
+++ src/ssg/sections/Header.js
@@ -1,7 +1,13 @@
-import { html } from '../../../lib/html-template-tag.js';
+import { html, css } from '../../../lib/html-template-tag.js';
 
 export const Header = (_props, _children) => html`
     <header>
-        <h1>Hello DLH!</h1>
+        <h1>Hello DLH!!</h1>
     </header>
+`;
+
+export const style = css`
+    h1 {
+        color: green;
+    }
 `;
