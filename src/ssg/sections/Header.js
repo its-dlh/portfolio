b83c80300b4b524e1da2065b3f095d9ef048e307
David L. Herring <david@fai2.com>
2022-01-31 19:20:14 -0500

    dlh logo

--- src/ssg/sections/Header.js
+++ src/ssg/sections/Header.js
@@ -1,8 +1,11 @@
 import { html, css } from '../../../lib/html-template-tag.js';
 
+import dlhLogo from '../../assets/images/dlh.svg';
+
 export const Header = (_props, _children) => html`
     <header>
         <h1>Hello DLH!!</h1>
+        <img src="${dlhLogo}">
     </header>
 `;
 
