230caf828e6bb84720a891a11d6b58356f7ab9b8
David L. Herring <david@fai2.com>
2022-03-02 18:52:21 -0500

    doing letter positioning as transforms

--- src/ssg/Logo.js
+++ src/ssg/Logo.js
@@ -4,22 +4,25 @@ import { html, css } from "../html-template-tag";
 export const Logo = () => html`
     <!-- I had a version of my logo that I've cobbled together for social media, but
          I figured this would look better if I just typed up an SVG. -->
-    <svg class="dlh-logo" width="145" height="100" viewBox="0 0 150 100" xmlns="http://www.w3.org/2000/svg">
+    <svg class="dlh-logo" width="150" height="100" viewBox="0 0 150 100" xmlns="http://www.w3.org/2000/svg">
         <!-- d -->
         <polygon
+            class="letter letter-d"
             points="55,0 55,100 0,100 0,50 35,50 35,70 20,70 20,80 35,80 35,0"
             fill="#000000"
         />
         
         <!-- L -->
         <polygon
-            points="85,0 85,80 115,80 115,100 65,100 65,0"
+            class="letter letter-l"
+            points="20,0 20,80 50,80 50,100 0,100 0,0"
             fill="#000000"
         />
         
         <!-- h -->
         <polygon
-            points="115,0 115,50 145,50 145,100 125,100 125,70 95,70 95,0"
+            class="letter letter-h"
+            points="20,0 20,50 50,50 50,100 30,100 30,70 0,70 0,0"
             fill="#000000"
         />
     </svg>
@@ -27,6 +30,12 @@ export const Logo = () => html`
 
 export const style = css`
     .dlh-logo {
+        .letter-l {
+            transform: translate(65px, 0);
+        }
         
+        .letter-h {
+            transform: translate(95px, 0);
+        }
     }
 `;
